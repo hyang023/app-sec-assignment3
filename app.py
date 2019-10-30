47e24b6c2a01ba9d38462fc6ac8b9cafead744bb
hyang023 <hwy227@nyu.edu>
2019-10-30 14:43:22 -0400

    Update app.py

--- app.py
+++ app.py
@@ -12,6 +12,13 @@ def create_app(config=None):
     @app.route('/')
     def hello_world():
         return 'Hello, World!'
+    
+    def init_db():
+    with app.app_context():
+        db = get_db()
+        with app.open_resource('schema.sql', mode='r') as f:
+            db.cursor().executescript(f.read())
+        db.commit()
 
     @app.route('/register', methods=['post', 'get'])
     def register():
