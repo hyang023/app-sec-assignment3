0e06d8a1b4698161f9f3d2932ede93358635b2e4
hyang023 <hwy227@nyu.edu>
2019-12-02 14:11:07 -0500

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from subprocess import check_output
 from flask import Flask, render_template, request
 from flask_sqlalchemy import SQLAlchemy
 
-#db = SQLAlchemy()
+db = SQLAlchemy()
 
 def create_app(config=None):
     app = Flask(__name__)
